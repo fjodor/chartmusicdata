c7fce6a03199746f2fe68b91ef772d14ade74825
fjodor <wolf.riepl@statistik-dresden.de>
2021-12-02 22:46:11 +0100

    data & data documentation

--- data-raw/data-prep.R
+++ data-raw/data-prep.R
@@ -5,15 +5,15 @@ library(here)
 
 version <- "0-3-0063"
 
-songs  <- read_csv(here("data-raw", paste0("chart2000-songmonth-",  version, ".csv")),
+songs2000  <- read_csv(here("data-raw", paste0("chart2000-songmonth-",  version, ".csv")),
                    na = c("", "-"))[]
 
-albums <- read_csv(here("data-raw", paste0("chart2000-albummonth-", version, ".csv")),
+albums2000 <- read_csv(here("data-raw", paste0("chart2000-albummonth-", version, ".csv")),
                    na = c("", "-"))[]
 
 # Sys.setenv(LANG = "en")
 
-songs <- songs %>%
+songs2000 <- songs2000 %>%
   mutate(month_org = month,
          month_date = yearmonth(month_org),
          month = month(month_date, label = TRUE, locale = "English"),
@@ -21,21 +21,14 @@ songs <- songs %>%
          year_month = format(yearmonth(month_org), "%Y-%m")) %>%
   select(year_month, month, year, -month_org, -month_date, position:au)
 
-attr(songs, "version") <- version
+attr(songs2000, "version") <- version
 
-albums <- albums %>%
+albums2000 <- albums2000 %>%
   mutate(month_org = month,
          month_date = yearmonth(month_org),
          month = month(month_date, label = TRUE, locale = "English"),
          year = year(month_date),
          year_month = format(yearmonth(month_org), "%Y-%m")) %>%
-  select(year_month, month, year, -month_org, -month_date, everything())
-
-albums <- albums %>%
-  mutate(         month_date = yearmonth(month_org),
-                  month = month(month_date, label = TRUE, locale = "English"),
-                  year = year(month_date),
-                  year_month = format(yearmonth(month_org), "%Y-%m")) %>%
   select(year_month, month, year, -month_org, -month_date, position:au)
 
-attr(albums, "version") <- version
+attr(albums2000, "version") <- version
