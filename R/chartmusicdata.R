2f06f442d6d8591fe4fa1e6b0fdd1b626665ccb6
fjodor <wolf.riepl@statistik-dresden.de>
2024-01-19 14:09:48 +0100

    updated package level documentation using usethis::use_package_doc()

--- R/chartmusicdata.R
+++
@@ -1,7 +0,0 @@
-#' chartmusicdata: A data package that contains data from https://chart2000.com and https://tsort.info.
-#'
-#' Datsets in this package: charts, topalbums, topsongs, albums2000, songs2000
-#'
-#' @docType _PACKAGE
-#' @name chartmusicdata
-NULL
